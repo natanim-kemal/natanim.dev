2c5a6832a3fea06e7c4d1883c5c28b67d9a42662
Natanim-Kemal <kemalnatanim@gmail.com>
2026-03-08 13:35:51 +0300

    optimize layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,32 +10,16 @@ import Contact from "@/components/Contact";
 export default function Home() {
   return (
     <main className="relative min-h-screen" style={{ backgroundColor: 'var(--background-base)' }}>
-      {/* Decorative Grid Background - z-index 0, behind everything */}
       <GridBackground />
-
-      {/* Navigation */}
       <Navbar />
-
-      {/* Content wrapper - z-index 1, above grid */}
       <div className="relative" style={{ zIndex: 1 }}>
-        {/* Hero Section */}
         <Hero />
-
-        {/* Tech Stack Marquee */}
         <div className="mt-4">
           <TechMarquee />
         </div>
-
-        {/* About Section */}
         <About />
-
-        {/* Work Section */}
         <Work />
-
-        {/* Open Source Section */}
         <OpenSource />
-
-        {/* Contact Section */}
         <Contact />
       </div>
     </main>
